5ad3868f3d8e55a0d2880421f29a40ead54aa732
buegare <brunobuegare@gmail.com>
2018-10-03 22:05:45 -0500

    Move api key to config file

--- src/app.js
+++ src/app.js
@@ -1,36 +1,43 @@
-import _ from 'lodash'
-import React, { Component } from 'react';
-import SearchBar from './components/search-bar'
-import YTSearch from 'youtube-api-search'
-import VideoList from './components/video-list'
-import VideoDetail from './components/video-detail'
+import _ from "lodash";
+import React, { Component } from "react";
+import SearchBar from "./components/search-bar";
+import YTSearch from "youtube-api-search";
+import VideoList from "./components/video-list";
+import VideoDetail from "./components/video-detail";
+import keys from "../config/keys";
 
-const API_KEY = 'AIzaSyBuTbaScsnhmAXXkiM3DVJKtMFWnNFVluU'
+const API_KEY = keys.API_KEY;
 
 class App extends Component {
   constructor(props) {
-    super(props)
+    super(props);
 
     this.state = {
       videos: [],
       selectedVideo: null
-    }
+    };
 
-    this.videoSearch('')
+    this.videoSearch("");
   }
 
   videoSearch(term) {
-    YTSearch({
-      key: API_KEY,
-      term: term
-    }, videos => this.setState({
-      videos: videos,
-      selectedVideo: videos[0]
-    }))
+    YTSearch(
+      {
+        key: API_KEY,
+        term: term
+      },
+      videos =>
+        this.setState({
+          videos: videos,
+          selectedVideo: videos[0]
+        })
+    );
   }
 
   render() {
-    const videoSearch = _.debounce(term => { this.videoSearch(term) }, 300)
+    const videoSearch = _.debounce(term => {
+      this.videoSearch(term);
+    }, 300);
 
     return (
       <div>
@@ -41,8 +48,8 @@ class App extends Component {
           onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
         />
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
